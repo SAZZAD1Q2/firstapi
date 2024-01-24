4c1a0022e73b74933b8bc94620428c8f544c1a7b
Md Sazzad Hossain <116729064+SAZZAD1Q2@users.noreply.github.com>
2024-01-24 19:19:52 +0600

    puma update

--- config/puma.rb
+++ config/puma.rb
@@ -25,6 +25,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT") { 3000 }
 
+
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
 
@@ -33,3 +34,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+preload_app!
